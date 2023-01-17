9b71f75b9d0a95a1959b3f59662766360547c6e0
vkats90 <mars77@gmail.com>
2023-01-17 08:51:10 -0700

    feat: checking functionality is working

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,52 @@
+function convertToArrays(puzzleString) {
+  let arr = puzzleString.split("");
+  let array = {
+    row: new Array(9).fill(".").map(() => new Array(9).fill(".")),
+    col: new Array(9).fill(".").map(() => new Array(9).fill(".")),
+    region: new Array(9).fill(".").map(() => new Array(9).fill(".")),
+  };
+  for (let i = 1; i < 10; i++) {
+    for (let j = 1; j < 10; j++) {
+      array.row[i - 1][j - 1] = arr[(i - 1) * 9 + j - 1];
+      array.col[j - 1][i - 1] = arr[(i - 1) * 9 + j - 1];
+      array.region[Math.floor((i - 1) / 3) * 3 + Math.floor((j - 1) / 3)][
+        ((i % 3 || 3) - 1) * 3 + (j % 3 || 3) - 1
+      ] = arr[(i - 1) * 9 + j - 1];
+    }
+  }
+  return array;
+}
 class SudokuSolver {
-
   validate(puzzleString) {
+    let arr = puzzleString.split[""];
+    if (arr.length() != 81) return "Expected puzzle to be 81 characters long";
+    if (/[^0-9\.]/.test(puzzleString)) return "Invalid characters in puzzle";
+  }
+
+  seeArray(puzzleString) {
+    return convertToArrays(puzzleString);
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    row = row.toLowerCase().charCodeAt(0) - 97;
+    column--;
+    return convertToArrays(puzzleString).row[row].includes(value);
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    row = row.toLowerCase().charCodeAt(0) - 97;
+    column--;
+    return convertToArrays(puzzleString).col[column].includes(value);
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-
+    row = row.toLowerCase().charCodeAt(0) - 97 + 1;
+    return convertToArrays(puzzleString).region[
+      Math.floor((row - 1) / 3) * 3 + Math.floor((column - 1) / 3)
+    ].includes(value);
   }
 
-  solve(puzzleString) {
-    
-  }
+  solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,39 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {
+    if (!req.body.coordinate || !req.body.value)
+      res.json({ error: "Required field(s) missing" });
+    else if (!/^[a-z][1-9]$/g.test(req.body.coordinate))
+      res.json({ error: "Invalid coordinate" });
+    else if (!/^[1-9]$/g.test(req.body.value))
+      res.json({ error: "Invalid value" });
+    else {
+      let row = req.body.coordinate.split("")[0];
+      let col = req.body.coordinate.split("")[1];
+      let conflict = [];
+      !solver.checkRowPlacement(req.body.puzzle, row, col, req.body.value)
+        ? undefined
+        : conflict.push("row");
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+      !solver.checkColPlacement(req.body.puzzle, row, col, req.body.value)
+        ? undefined
+        : conflict.push("column");
 
-    });
+      !solver.checkRegionPlacement(req.body.puzzle, row, col, req.body.value)
+        ? undefined
+        : conflict.push("region");
+
+      res.json({
+        valid: conflict.length == 0,
+        conflict: conflict.length == 0 ? undefined : conflict,
+      });
+    }
+  });
+
+  app.route("/api/solve").post((req, res) => {});
 };
